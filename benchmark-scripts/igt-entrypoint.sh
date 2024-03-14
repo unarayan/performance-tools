4f55aa045e906e9bb4d04640ba2303451d7f1298
Brian McGinn <brian.mcginn@intel.com>
2024-03-14 10:05:59 -0700

    fix: reviewdog issues and pr comments
    Signed-off-by: Brian McGinn <brian.mcginn@intel.com>

--- benchmark-scripts/igt-entrypoint.sh
+++ benchmark-scripts/igt-entrypoint.sh
@@ -1,22 +1,27 @@
 #!/usr/bin/env bash
 #
-# Copyright (C) 2023 Intel Corporation.
+# Copyright (C) 2024 Intel Corporation.
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# shellcheck disable=SC2046 # Intended work splitting
 devices=$(/usr/local/bin/intel_gpu_top -L  | grep card | awk '{print $1,$5;}' | sed ":a;N;s/\n/@/g")
 echo "devices found $devices"
 
 IFS='@'
 for device in $devices
 do
+    # shellcheck disable=SC2046 # Intended work splitting
     dev=$(echo $device | awk '{print $1}')
     echo "$dev"
+    # shellcheck disable=SC2046 # Intended work splitting
     deviceId=$(echo $device | awk '{print $2}' | sed -E 's/.*?device=//' | cut -f1 -d",")
     echo "$deviceId"
+    # shellcheck disable=SC2046 # Intended work splitting
     deviceNum=$(echo $dev | sed -E 's/.*?card//')
     echo "device number: $deviceNum"
+    # shellcheck disable=SC2046 # Intended work splitting
     /usr/local/bin/intel_gpu_top -d pci:card=$deviceNum -J -s 1000 > /tmp/results/igt$deviceNum-$deviceId.json &
     echo "Starting igt capture for $device in igt$deviceNum-$deviceId.json"
 done
