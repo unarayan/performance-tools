f40c44e34d09b4a1f0f4d69a1c09025ca988ba00
Brian McGinn <brian.mcginn@intel.com>
2024-03-22 14:29:35 -0700

    fix: pcm-memory and pcm-power are only supported for xeon platforms. Added a check to only run these logs on xeon platforms
    Signed-off-by: Brian McGinn <brian.mcginn@intel.com>

--- docker/platform/entrypoint.sh
+++ docker/platform/entrypoint.sh
@@ -22,7 +22,22 @@ touch /tmp/results/disk_bandwidth.log
 chown 1000:1000 /tmp/results/disk_bandwidth.log
 iotop -o -P -b >& /tmp/results/disk_bandwidth.log &
 
-echo "Starting xeon pcm-power collection"
+is_xeon=`lscpu | grep -i xeon | wc -l`
+
+if [ "$is_xeon"  == "1"  ]
+  then
+    echo "Starting pcm-memory collection"
+    touch /tmp/results/pcm-memory.csv
+    chown 1000:1000 /tmp/results/pcm-memory.csv
+    /opt/intel/pcm-bin/bin/pcm-memory 1 -silent -nc -csv=/tmp/results/pcm-memory.csv &
+
+    echo "Starting pcm-power collection"
+    touch /tmp/results/pcm-power.log
+    chown 1000:1000 /tmp/results/pcm-power.log
+    /opt/intel/pcm-bin/bin/pcm-power >& /tmp/results/pcm-power.log &
+  fi
+
+echo "Starting general pcm collection"
 touch /tmp/results/pcm.csv
 chown 1000:1000 /tmp/results/pcm.csv
 /opt/intel/pcm-bin/bin/pcm 1 -silent -nc -nsys -csv=/tmp/results/pcm.csv
