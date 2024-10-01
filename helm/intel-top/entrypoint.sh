8af63db332a87bf6892fd728ab55a9e29e0625c7
seanohair22 <sean.ohair@intel.com>
2024-10-01 14:14:54 -0700

    feat: implement minikube and helm deployment (#62)
    Signed-off-by: Sean O'Hair <sean.ohair@intel.com>

---
+++ helm/intel-top/entrypoint.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2024 Intel Corporation.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# shellcheck disable=SC2086 # Intended work splitting
+devices=$(intel_gpu_top -L  | grep card | awk '{print $1,$5;}' | sed ":a;N;s/\n/@/g")
+echo "devices found $devices"
+
+if [ -z "$devices" ]
+then
+    echo "No valid GPU devices found"
+	exit 1
+fi
+
+IFS='@'
+for device in $devices
+do
+    # shellcheck disable=SC2086 # Intended work splitting
+    dev=$(echo $device | awk '{print $1}')
+    echo "$dev"
+    # shellcheck disable=SC2086 # Intended work splitting
+    deviceId=$(echo $device | awk '{print $2}' | sed -E 's/.*?device=//' | cut -f1 -d",")
+    echo "$deviceId"
+    # shellcheck disable=SC2086 # Intended work splitting
+    deviceNum=$(echo $dev | sed -E 's/.*?card//')
+    echo "device number: $deviceNum"
+    touch /tmp/results/igt$deviceNum-$deviceId.csv
+    chown 1000:1000 /tmp/results/igt$deviceNum-$deviceId.csv
+    # shellcheck disable=SC2086 # Intended work splitting
+    intel_gpu_top -d pci:card=$deviceNum -c -o /tmp/results/igt$deviceNum-$deviceId.csv &
+    echo "Starting igt capture for $device in igt$deviceNum-$deviceId.csv"
+done
+
+while true
+do
+	echo "Capturing igt metrics"
+	sleep 15
+done
---
+++ helm/platform/entrypoint.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2024 Intel Corporation.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+echo "Starting platform data collection"
+
+echo "Starting sar collection"
+touch /tmp/results/cpu_usage.log
+chown 1000:1000 /tmp/results/cpu_usage.log
+sar 1 >& /tmp/results/cpu_usage.log &
+
+echo "Starting free collection"
+touch /tmp/results/memory_usage.log
+chown 1000:1000 /tmp/results/memory_usage.log
+free -s 1 >& /tmp/results/memory_usage.log &
+
+echo "Starting iotop collection"
+touch /tmp/results/disk_bandwidth.log
+chown 1000:1000 /tmp/results/disk_bandwidth.log
+iotop -o -P -b >& /tmp/results/disk_bandwidth.log &
+
+is_xeon=`lscpu | grep -i xeon | wc -l`
+
+if [ "$is_xeon"  == "1"  ]
+  then
+    echo "Starting pcm-memory collection"
+    touch /tmp/results/pcm-memory.csv
+    chown 1000:1000 /tmp/results/pcm-memory.csv
+    /opt/intel/pcm-bin/bin/pcm-memory 1 -silent -nc -csv=/tmp/results/pcm-memory.csv &
+
+    echo "Starting pcm-power collection"
+    touch /tmp/results/pcm-power.log
+    chown 1000:1000 /tmp/results/pcm-power.log
+    /opt/intel/pcm-bin/bin/pcm-power >& /tmp/results/pcm-power.log &
+  fi
+
+echo "Starting general pcm collection"
+touch /tmp/results/pcm.csv
+chown 1000:1000 /tmp/results/pcm.csv
+/opt/intel/pcm-bin/bin/pcm 1 -silent -nc -nsys -csv=/tmp/results/pcm.csv
